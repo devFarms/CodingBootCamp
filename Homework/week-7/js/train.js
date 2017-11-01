76c1a3df141825968e4fd05c3a714970fc11fe8c
Alex Green <agreen@santa-ana.org>
2017-11-01 12:09:04 -0700

    Still working on the next train section

--- Homework/week-7/js/train.js
+++ Homework/week-7/js/train.js
@@ -8,6 +8,7 @@ var trainName = "";
 var trainDestination = "";
 var trainFirst = "";
 var trainFrequency = 0;
+var trainNext = 0;
 
 // Calculated variables
 var nextTrain = 0;
@@ -15,13 +16,34 @@ var minutesAway = 0;
 
 var firebaseRef = firebase.database().ref();
 
+
+
 firebaseRef.on("child_added", snap => {
 	var trainName = snap.child("trainName").val();
 	var trainDestination = snap.child("trainDestination").val();
 	var trainFirst = snap.child("trainFirst").val();
 	var trainFrequency = snap.child("trainFrequency").val();
 
-	$("#train-timetable").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>Every " + trainFrequency + " minutes</td><td>" + trainFirst + "</td><td>" + moment().format('LT') + "</td><td><button type='button' class='btn btn-danger'>Cancel Train</button></td></tr>")
+/*  How to calculate trainNext 
+
+Take trainFirst and add 45 minutes to each loop. Check the product against the current time. If 
+
+trainNext = currentTime + 45 minutes.
+If trainNext is less than currentTime add another 45 minutes.
+Else, display value of trainNext
+
+*/
+
+    // var trainNext = ;
+    // var trainNext = new Date(moment().add(45, 'm').toDate() * 1000);
+    var currentTime = new Date(),
+        trainNext = new Date (currentTime);
+        trainNext.setMinutes(currentTime.getMinutes() + /* trainFrequency */ 1440);
+    console.log(trainFrequency);
+    console.log(trainNext);
+
+
+	$("#train-timetable").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>Every " + trainFrequency + " minutes</td><td>" + trainNext + "</td><td>Minutes Away Calc Here</td><td><button type='button' class='btn btn-danger'>Cancel Train</button></td></tr>")
 });
 
 
