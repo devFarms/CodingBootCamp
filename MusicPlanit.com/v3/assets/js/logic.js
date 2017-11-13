0aaeb2f6a5b24427d90ad2a4999f4d631efaa604
Alex Green <alex@devfarms.com>
2017-11-13 09:19:32 -0800

    Started version 3 of Music Planit.

---
+++ MusicPlanit.com/v3/assets/js/logic.js
@@ -0,0 +1,13 @@
+console.log("I'm here.");
+
+$(document).on("click", "#btn-search-city", function() {
+        $("html,body").animate({
+        scrollTop: $("#two").offset().top},
+        "slow");
+    });
+
+$(document).on("click", ".eventElemLg", function() {
+        $("html,body").animate({
+        scrollTop: $("#two").offset().top},
+        "slow");
+    });
