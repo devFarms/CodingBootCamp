87edc245be920995406cdadbb680bd5f69447fc9
Alex Green <agreen@santa-ana.org>
2017-11-14 17:16:10 -0800

    Arghgit add -A How do I get the city and country out of Google Places!

--- MusicPlanit.com/v3/assets/js/logic.js
+++ MusicPlanit.com/v3/assets/js/logic.js
@@ -1,7 +1,12 @@
 // Google Maps API Key : AIzaSyAHZTkBCcadYHRcvz0_xGnftVtDCjaQH0k
 
+
+var input = ""
+var autocomplete = [];
+
 console.log('Ready...');
 
+/*
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -10,27 +15,29 @@ $('.datepicker').pickadate({
     close: 'Ok',
     closeOnSelect: true // Close upon selecting a date,
   });
-
+*/
 function activatePlacesSearch(){
 
-	var input = document.getElementById('input-search-city');
-	var autocomplete = new google.maps.places.Autocomplete(input);
+    input = document.getElementById('input-search-city');
+	autocomplete = new google.maps.places.Autocomplete(input);
+    
+};
 
-	$(document).on('click', '#btn-search-city', function(e) {
-        e.preventDefault();
-        $('html,body').animate({
-        scrollTop: $('#two').offset().top},
-        'slow');
-        
-        input = $('#input-search-city').val().trim();
-        console.log(input.split(','));
+$(document).on('click', '#btn-search-city', function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+    scrollTop: $('#two').offset().top},
+    'slow');
+    
+    input = $('#input-search-city').val().trim();
+    console.log(input.split(','));
+    //console.log(autocomplete);
 
-        var startDate = $('#start-date').val().trim();
-        var endDate = $('#end-date').val().trim();
-        console.log(startDate);
-        console.log(endDate);
-    });
-}
+    var startDate = $('#start-date').val().trim();
+    var endDate = $('#end-date').val().trim();
+    console.log(startDate);
+    console.log(endDate);
+});
 
 $(document).on('click', '.card', function(e) {
         e.preventDefault();
